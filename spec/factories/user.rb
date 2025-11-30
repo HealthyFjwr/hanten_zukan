c33d5f831db4182fdac43d310af50cb991be2f07
わら <fjwr.0401@gmail.com>
2025-11-30 23:20:56 +0900

    chore: rspec-rails を Gemfile に追加

---
+++ spec/factories/user.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    name {"テスト太郎"}
+    email {Faker::Internet.email}
+    password { "password123" }
+    password_confirmation  { "password123" }
+  end
+end
+
