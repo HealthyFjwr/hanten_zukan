a7a76e68fbfbaed1fb39c11f39227cecf5b6a88f
わら <fjwr.0401@gmail.com>
2026-02-14 00:43:39 +0900

    add: rspec 管理者アカウントでのログインテスト

---
+++ spec/requests/admin/restaurants_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe "Admin::Restaurants", type: :request do
+  describe "GET /admin/restaurants" do
+    context "未ログイン" do
+      it "ログイン画面にリダイレクトされる" do
+        get "/admin/restaurants"
+        expect(response).to have_http_status(:found) # 302
+      end
+    end
+
+    context "ログイン済み" do
+      let(:admin) { create(:admin) }
+
+      it "200で表示される" do
+        sign_in admin
+        get "/admin/restaurants"
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+end
