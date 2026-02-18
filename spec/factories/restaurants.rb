6b9afe02bd5ba978d6b9a29e87b56c2bd8325935
わら <fjwr.0401@gmail.com>
2026-02-19 02:36:26 +0900

    test: モデルスペック

---
+++ spec/factories/restaurants.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :restaurant do
+    sequence(:place_id) { |n| "place_#{n}" }
+    sequence(:name) { |n| "南海飯店#{n}" }
+    address { "大阪府大阪市" }
+    latitude { BigDecimal("34.6937") }
+    longitude { BigDecimal("135.5023") }
+    phone_number { "06-0000-0000" }
+    website { "https://example.com" }
+    rating { BigDecimal("4.2") }
+    user_rating_count { 123 }
+    opening_hours { { "weekday_text" => ["月: 11:00-20:00"] } }
+  end
+end
+
---
+++ spec/models/admin_user_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe AdminUser, type: :model do
+  it "factoryが有効" do
+    expect(build(:admin_user)).to be_valid
+  end
+
+  it "emailはユニーク" do
+    create(:admin_user, email: "a@example.com")
+    dup = build(:admin_user, email: "a@example.com")
+    expect(dup).not_to be_valid
+  end
+end
