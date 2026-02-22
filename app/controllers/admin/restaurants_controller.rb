ab5c9a15ac2905ef6cfa39f0a5bddda1a1052e86
わら <fjwr.0401@gmail.com>
2026-02-23 01:45:18 +0900

    add: 管理者画面店舗一覧と店舗詳細ページ

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,7 +1,30 @@
 # frozen_string_literal: true
 
+
 module Admin
   class RestaurantsController < Admin::BaseController
-    def index; end
+    before_action :set_q, only: [:index]
+
+    def index
+      @restaurants = @q.result(distinct: true).order(created_at: :desc)
+    end
+
+    def show
+      @restaurant = Restaurant.find(params[:id])
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def set_q
+      @q = Restaurant.ransack(params[:q])
+    end
+
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :address, :latitude, :longitude)
+    end
   end
 end
