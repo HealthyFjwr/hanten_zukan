485009e6b54f88d638bd5d421957949aed194d7d
わら <fjwr.0401@gmail.com>
2026-03-18 22:52:47 +0900

    add: ブックマークページまでの導線

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -2,6 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="dropdown"
 export default class extends Controller {
-  connect() {
+  static targets = ["menu"]
+
+  toggle(event) {
+    event.stopPropagation()
+    this.menuTarget.classList.toggle("hidden")
+  }
+
+  hide() {
+    this.menuTarget.classList.add("hidden")
   }
 }
