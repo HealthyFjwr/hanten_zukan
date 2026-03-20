80b616469cbf1aa38d18ee3517ac60f5f5e2c989
わら <fjwr.0401@gmail.com>
2026-03-20 19:40:17 +0900

    fix: lint error

--- spec/factories/bookmarks.rb
+++ spec/factories/bookmarks.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :bookmark do
-    user { nil }
-    restaurant { nil }
+    association :user
+    association :restaurant
   end
 end
